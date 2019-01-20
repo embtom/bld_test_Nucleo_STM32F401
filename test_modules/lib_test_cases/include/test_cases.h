af10beb8d5420bbd88941ada950df3b48293e557
tom3333 <t.willetal@googlemail.com>
2019-01-20 08:04:52 +0100

    update test cases

--- test_modules/lib_test_cases/include/test_cases.h
+++ test_modules/lib_test_cases/include/test_cases.h
@@ -1,18 +1,26 @@
-/* ****************************************************************************************************
- * test_cases_init.c within the following project: lib_thread
- *
- *  compiler:   GNU Tools ARM LINUX
- *  target:     armv6
- *  author:	    Tom
- * ****************************************************************************************************/
-
-/* ****************************************************************************************************/
-
 /*
- *	******************************* change log *******************************
- *  date			user			comment
- * 	11.09.2018			Tom			- creation of test_cases.h
+ * This file is part of the EMBTOM project
+ * Copyright (c) 2018-2019 Thomas Willetal 
+ * (https://github.com/tom3333)
  *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
 #ifndef _TEST_CASES_H_
@@ -90,8 +98,6 @@ int test_cases__register(test_case_instance_t *_test_case);
  * \return	pointer to list if successful, NULL if not
  * ******************************************************************/
 struct queue_attr* test_cases__get_list(void);
-
-
 
 #ifdef __cplusplus
 }
